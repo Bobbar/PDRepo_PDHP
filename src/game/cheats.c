a3c736dd723489367a5bac0d9e1f196651917e36
Bobby Lovell <rblovell@fairfielddd.com>
2024-01-11 13:02:14 -0500

    Add unlock everything, K7 alt-fire muzzle flash, inf ammo psychosis gun.

--- src/game/cheats.c
+++ src/game/cheats.c
@@ -481,6 +481,77 @@ char *cheatGetName(s32 cheat_id)
 	return langGet(g_Cheats[cheat_id].nametextid);
 }
 
+static MenuItemHandlerResult menuhandlerUnlockEverything(s32 operation, struct menuitem* item, union handlerdata* data)
+{
+	if (operation == MENUOP_SET) {
+		gamefileUnlockEverything();
+	}
+	return 0;
+}
+
+struct menuitem g_CheatsConfirmUnlockMenuItems[] = {
+	{
+		MENUITEMTYPE_LABEL,
+		0,
+		MENUITEMFLAG_LITERAL_TEXT,
+		(uintptr_t)"Are you sure?\n\nThis will overwrite any progress\nsaved to the current profile.\n",
+		0,
+		NULL,
+	},
+	{
+		MENUITEMTYPE_SEPARATOR,
+		0,
+		0,
+		0x00000082,
+		0,
+		NULL,
+	},
+	{
+		MENUITEMTYPE_MARQUEE,
+		0,
+		MENUITEMFLAG_SMALLFONT | MENUITEMFLAG_MARQUEE_FADEBOTHSIDES | MENUITEMFLAG_LITERAL_TEXT,
+		(uintptr_t)"Unlocks all cheats, weapons, missions, challenges and combat simulator items.\n",
+		0,
+		NULL,
+	},
+	{
+		MENUITEMTYPE_SEPARATOR,
+		0,
+		0,
+		0x00000082,
+		0,
+		NULL,
+	},
+	{
+		MENUITEMTYPE_SELECTABLE,
+		0,
+		MENUITEMFLAG_SELECTABLE_CENTRE | MENUITEMFLAG_SELECTABLE_CLOSESDIALOG,
+		L_OPTIONS_191, // "No"
+		0,
+		NULL,
+	},
+	{
+		MENUITEMTYPE_SELECTABLE,
+		0,
+		MENUITEMFLAG_SELECTABLE_CENTRE | MENUITEMFLAG_SELECTABLE_CLOSESDIALOG,
+		L_OPTIONS_190, // "Yes"
+		0,
+		menuhandlerUnlockEverything,
+	},
+	{ MENUITEMTYPE_END },
+};
+
+
+struct menudialogdef g_CheatsConfirmUnlockMenuDialog = {
+	MENUDIALOGTYPE_DANGER,
+	L_OPTIONS_188, // "Warning"
+	g_CheatsConfirmUnlockMenuItems,
+	NULL,
+	0,
+	NULL,
+};
+
+
 struct menuitem g_CheatsFunMenuItems[] = {
 	{
 		MENUITEMTYPE_CHECKBOX,
@@ -1159,6 +1230,14 @@ struct menuitem g_CheatsMenuItems[] = {
 		L_MPWEAPONS_217, // "Turn off all Cheats"
 		0,
 		cheatMenuHandleTurnOffAllCheats,
+	},
+	{
+		MENUITEMTYPE_SELECTABLE,
+		0,
+		MENUITEMFLAG_LITERAL_TEXT | MENUITEMFLAG_SELECTABLE_OPENSDIALOG,
+		(uintptr_t)"Unlock Everything\n",
+		0,
+		(void*)&g_CheatsConfirmUnlockMenuDialog,
 	},
 	{
 		MENUITEMTYPE_SEPARATOR,
